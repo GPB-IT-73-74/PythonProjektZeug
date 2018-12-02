aabc5df7e28657ad6a1770a33cd1579a88dded5f
m2dan <45355464+m2dan@users.noreply.github.com>
2018-12-02 17:14:29 +0100

    Add Readme.md for Toolkit

---
+++ toolkit_README.md.py
@@ -0,0 +1,29 @@
+Members:
+    
+Lukas Jurczak
+LukasJurc
+https://github.com/LukasJurc
+
+Dinko Eichin
+samurai.codes
+https://github.com/samuraicodes
+
+Furkan Karakus
+Black
+https://github.com/Black00Z
+
+Stephan  Korsitzky
+StephanOn
+https://github.com/StephanOn
+
+Mussa Sweidan
+m2dan
+https://github.com/m2dan
+
+
+Project: toolkit
+Link to master repo: GitHub Repository: https://github.com/LukasJurc/toolkit
+
+Description:
+The program consists of 4 parts. The first part is a library where students can get a quick acces to several programming languages. The second program calculates your average GPA.
+Inside the 3rd program, you can insert quick notes that will be stored in a note folder inside the application. The last application opens the webbrowser and lets you read the documentation in pdf.
